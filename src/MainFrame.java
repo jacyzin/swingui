4794fb99cad66b08c9f9cbf65dd81e29f50dd0b1
Henry Jacyzin <henryjacyzin@gmail.com>
2018-06-26 19:54:45 -0300

    Add menu

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
@@ -22,34 +25,37 @@ public class MainFrame extends JFrame {
 		btn1 = new JButton("Click Me");
 		formPanel = new FormPanel();
 		
+		setJMenuBar(createMenuBar());
+
 		toolbar.setStringListener(new StringListener() {
 
 			@Override
 			public void textEmitted(String text) {
 				textPanel.append(text);
 			}
-			
+
 		});
-		
+
 		formPanel.setFormListener(new FormListener() {
 			public void formEventOccurred(FormEvent e) {
 				String name = e.getName();
 				String occupation = e.getOccupation();
 				int ageCat = e.getAgeCat();
 				String empCat = e.getEmpCategory();
-				
-				textPanel.append(name + ": " + occupation + " : " + ageCat + " :" + empCat + " : " + e.getGender() + "\n");
+
+				textPanel.append(
+						name + ": " + occupation + " : " + ageCat + " :" + empCat + " : " + e.getGender() + "\n");
 			}
 		});
-		
+
 		btn1.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				textPanel.append("Hello !!!!\n");
 			}
 		});
-		
+
 		add(textPanel, BorderLayout.CENTER);
 		add(toolbar, BorderLayout.NORTH);
 		add(btn1, BorderLayout.SOUTH);
@@ -59,6 +65,31 @@ public class MainFrame extends JFrame {
 		setSize(800, 700);
 		setVisible(true);
 
+	}
+
+	private JMenuBar createMenuBar() {
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem exportDataItem = new JMenuItem("Export Data..");
+		JMenuItem importDataItem = new JMenuItem("Import Data..");
+		JMenuItem exitItem = new JMenuItem("Exit");
+		
+		fileMenu.add(exportDataItem);
+		fileMenu.add(importDataItem);
+		fileMenu.addSeparator();
+		fileMenu.add(exitItem);
+		
+		JMenu windowMenu = new JMenu("Window");
+		JMenu showMenu = new JMenu("Show");
+		JMenuItem showFormItem = new JMenuItem("Person Form");
+		showMenu.add(showFormItem);
+		windowMenu.add(showMenu);
+		
+		menu.add(fileMenu);
+		menu.add(windowMenu);
+		
+		return menu;
 	}
 
 }
